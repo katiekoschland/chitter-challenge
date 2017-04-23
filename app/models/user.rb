5255e9cc389729303dc91b44fe285990ff53fd33
Katie Koschland <katiekoschland@hotmail.com>
2017-04-23 15:27:12 +0100

    Password confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ require 'data_mapper'
 require 'dm-postgres-adapter'
 
 class User
+
+  attr_reader :password
+  attr_accessor :password_confirmation
+
   include DataMapper::Resource
 
   property :id, Serial
@@ -10,7 +14,10 @@ class User
   property :password_digest, Text
 
   def password=(password)
+    @password = password
     self.password_digest = BCrypt::Password.create(password)
   end
 
+
+  validates_confirmation_of :password
 end
--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -2,9 +2,16 @@ require 'web_helper'
 require './app/models/user'
 
 feature 'User sign up' do
+
   scenario 'I can sign up as a new user' do
     expect { sign_up }.to change(User, :count).by(1)
     expect(page).to have_content('Welcome, test@example.com')
     expect(User.first.email).to eq('test@example.com')
   end
-end
+
+
+    scenario 'requires a matching confirmation password' do
+      expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
+    end
+
+  end
