acafaf23a0a681e917afb70fb2604db83c085323
Katie Koschland <katiekoschland@hotmail.com>
2017-04-23 15:37:23 +0100

    Use Sinatra Flash to display message to user when the passwords do not match

--- app/app.rb
+++ app/app.rb
@@ -1,28 +1,35 @@
 ENV['RACK_ENV'] ||= 'development'
 
 require 'sinatra/base'
+require 'sinatra/flash'
 require_relative 'models/user'
 require_relative 'data_mapper_setup'
-
 
 class Chitter < Sinatra::Base
   enable :sessions
   set :session_secret, 'super secret'
+  register Sinatra::Flash
 
   get '/' do
     erb :'index'
   end
 
   get '/users/new' do
+    @user = User.new
     erb :'users/new'
   end
 
   post '/users' do
-    user = User.create(email: params[:email],
-    password: params[:password],
-    password_confirmation: params[:password_confirmation])
-    session[:user_id] = user.id
-    redirect to('/')
+    @user = User.new(email: params[:email],
+                    password: params[:password],
+                    password_confirmation: params[:password_confirmation])
+    if @user.save
+      session[:user_id] = @user.id
+      redirect to('/')
+    else
+      flash.now[:notice] = "Password and confirmation password do not match"
+      erb :'users/new'
+    end
   end
 
   helpers do
